cdd380cd7e53df5c2cf72583c83a60723d2659bc
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-04-08 15:29:25 -0500

    Update

--- Backend/MarketPlace_Backend/marketplace/models/productor.py
+++ Backend/MarketPlace_Backend/marketplace/models/productor.py
@@ -5,5 +5,8 @@ class Productor(models.Model):
     nombre = models.CharField(max_length=150, null=False, blank=False)
     direccion = models.CharField(max_length=200, null=False, blank=False)
     telefono = models.CharField(max_length=20, null=False, blank=False)
+    activado = models.BooleanField(default=False, blank=False, null=False)
+    foto = models.ImageField(upload_to='static', null=True, blank=True)
+
 
 
