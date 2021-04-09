2fc8f6ea2aa596340c316f3009e846738338fe13
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-04-09 15:32:53 -0500

    Se agregaron las referencias a los modelos many to many de leonardo

--- Backend/MarketPlace_Backend/marketplace/models/producto_catalogo.py
+++ Backend/MarketPlace_Backend/marketplace/models/producto_catalogo.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .canasta import canasta as r_canasta
+from .cantidad_producto_catalogo import cantidad_producto_catalogo as r_cantidad_producto_catalogo
 
 class producto_catalogo(models.Model):
     nombre = models.CharField(max_length=30, null=False, blank=False)
@@ -8,6 +9,7 @@ class producto_catalogo(models.Model):
     unidad = models.CharField(max_length=20, null=False, blank=False)
     activado = models.BooleanField(blank=False, null=False)
     canasta = models.ForeignKey(r_canasta, on_delete= models.DO_NOTHING, blank=True, null=True)
+    cantidad_producto_catalogo = models.ManyToManyField(r_cantidad_producto_catalogo)
 
 
     
