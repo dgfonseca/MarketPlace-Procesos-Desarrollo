b670b7f766da3f1c87289cbd716b7021b9974cf4
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-05-01 11:47:16 -0500

    Merge conflict with GM-71 Solved

--- Backend/Backend/tests.py
+++ Backend/Backend/tests.py
@@ -17,6 +17,27 @@ class GM71Tests(TestCase):
         request_delete = self.client.delete('/api/producto-catalogo/' + str(id_producto_catalogo)+'/')
         self.assertEqual(request_delete.status_code, 204)
 
+        # test que no elimina correctamente dado que hay ofertas del producto
+        request_post_producto_catalogo = self.client.post('/api/producto-catalogo/', json.dumps(
+            {
+                "nombre": "taquitos de canasta",
+                "precioPorUnidad": 1500,
+                "fotoProducto": "Fotoasdsad",
+                "unidad": "kg",
+                "activado": False
+            }
+        ), content_type="application/json")
+        id_producto_catalogo =json.loads(request_post_producto_catalogo.content)["id"]
+        request_post_producto = self.client.post('/api/producto/', json.dumps(
+            {
+                    "precioPorUnidad":"23.3",
+                    "cantidadDisponible":"20",
+                    "productoCatalogo": id_producto_catalogo
+            }
+        ), content_type="application/json")
+        request_delete = self.client.delete('/api/producto-catalogo/' + str(id_producto_catalogo)+'/')
+        self.assertEqual(request_delete.status_code, 406)
+
 class GM_09_Tests(TestCase):
     def test_post_producto_catalogo(self):
         response = self.client.post('/api/producto-catalogo/', json.dumps(
@@ -241,23 +262,3 @@ class GM_09_Tests(TestCase):
 
 
 
-        # test que no elimina correctamente dado que hay ofertas del producto
-        request_post_producto_catalogo = self.client.post('/api/producto-catalogo/', json.dumps(
-            {
-                "nombre": "taquitos de canasta",
-                "precioPorUnidad": 1500,
-                "fotoProducto": "Fotoasdsad",
-                "unidad": "kg",
-                "activado": False
-            }
-        ), content_type="application/json")
-        id_producto_catalogo =json.loads(request_post_producto_catalogo.content)["id"]
-        request_post_producto = self.client.post('/api/producto/', json.dumps(
-            {
-                    "precioPorUnidad":"23.3",
-                    "cantidadDisponible":"20",
-                    "productoCatalogo": id_producto_catalogo
-            }
-        ), content_type="application/json")
-        request_delete = self.client.delete('/api/producto-catalogo/' + str(id_producto_catalogo)+'/')
-        self.assertEqual(request_delete.status_code, 406)
